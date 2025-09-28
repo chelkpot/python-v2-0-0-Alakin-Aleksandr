0613dc47ec738cf275453f49da191b56dc62330b
Alakin-Aleksandr <alakinsasa2008@gmail.com>
2025-09-28 22:46:15 +0500

    Add files via upload
    исправил ошибку

--- tasks/task2.py
+++ tasks/task2.py
@@ -2,7 +2,7 @@
 
 def solve():
 # Ниже пишите решение задачи
-    x, y, z = map(int, input("Введите кол-во карандашей, ручек и фламастеров: ").split())
+    x, y, z = map(int, input().split())
     print(x*3+y*5+z*12)
 
    
