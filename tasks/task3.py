acc9f9e08d8f5a52d87e8cbd80b407dfd4185b0b
Alakin-Aleksandr <alakinsasa2008@gmail.com>
2025-09-28 22:35:15 +0500

    Add files via upload
    Сделал дз

--- tasks/task3.py
+++ tasks/task3.py
@@ -2,9 +2,9 @@
 
 def solve():
 # Ниже пишите решение задачи
-    
-
-
+    a, b = map(int, input("Введите количество банок: ").split())
+    total = a + b - 1
+    print(total - a, total - b)
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
     solve()
